35a7b3e2192937a77af398f8aecc5b21cea9133f
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2024-07-28 21:01:42 +0300

    Add project files.

---
+++ Rock paper scissors dotnet/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Rock_paper_scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] choices = { "rock", "paper", "scissors" };
+
+            Console.WriteLine("ROCK PAPER SCISSORS Mini-Game!");
+
+            string userSelection = FindUserSelection(choices);
+
+            Random random = new Random();
+            int npcChoiceIndex = random.Next(choices.Length);
+            string npcChoice = choices[npcChoiceIndex];
+
+            Console.WriteLine($"NPC chose: {npcChoice}");
+
+            string result = WinnerSelection(userSelection, npcChoice);
+            Console.WriteLine(result);
+        }
+
+        static string FindUserSelection(string[] choices)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your choice (rock, paper, or scissors): ");
+                string userSelection = Console.ReadLine().ToLower();
+
+                if (choices.Contains(userSelection))
+                {
+                    return userSelection;
+                }
+
+                Console.WriteLine($"There is no such selection as \"{userSelection}\". Please enter rock, paper, or scissors.");
+            }
+        }
+
+        static string WinnerSelection(string userSelection, string npcChoice)
+        {
+            if (userSelection == npcChoice)
+            {
+                return "It's a tie";
+            }
+            else if ((userSelection == "rock" && npcChoice == "scissors") ||
+                    (userSelection == "paper" && npcChoice == "rock") ||
+                    (userSelection == "scissors" && npcChoice == "paper"))
+            {
+                return "You WON";
+            }
+            else
+            {
+                return "You lost. Try again";
+            }
+        }
+    }
+}
